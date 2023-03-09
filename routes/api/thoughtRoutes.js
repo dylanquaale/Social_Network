75203732d97c80dda2545f872e5af03c7f63d6e0
dylanquaale <dylanquaale33@gmail.com>
2023-03-09 11:18:20 -0600

    thoughts routes created

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,8 +1,23 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
+// all of the requirements
 const {
-    getAllThoughts,
+  getAllThoughts,
+  createNewThought,
+  getThoughtsById,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtsController");
 
-} = require('../../controllers/thoughtsController');
+// get/create thoughts
+router.route("/").get(getAllThoughts).post(createNewThought);
+
+// find thoughts by id activity 23 is what I am goin off
+router.route("/:id").get(getThoughtsById).put(updateThought).delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
