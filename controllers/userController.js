cf098f090b76127e2e1ba72304b8adcd9cfe6397
dylanquaale <dylanquaale33@gmail.com>
2023-03-09 12:49:47 -0600

    working in userControllers

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const User = require('../models/User');
+
+module.exports = {
+    getAllUsers(req, res) {
+        User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
+    },
+    // get one user
+    getUserById(req, res) {
+        User.findOne({ _id: req.params.userId})
+        .select('-__v')
+        .then((user) => 
+            !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    // createUser
+    createNewUser(req, res) {
+        User.create(req.body)
+          .then((dbUserData) => res.json(dbUserData))
+          .catch((err) => res.status(500).json(err));
+      },
+    };
+    
