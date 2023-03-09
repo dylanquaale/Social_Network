6f3c9b341f6584107697b62623fefb759e124ab4
dylanquaale <dylanquaale33@gmail.com>
2023-03-09 11:28:26 -0600

    user route made

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+
+const {
+    getAllUsers,
+    createNewUser,
+    getUsersById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+router.route('/').get(getAllUsers).post(createNewUser);
+router.route('/:id').get(getUsersById).put(updateUser).delete(deleteUser);
+router.route('/:userId/friends/friendId').post(addFriend).delete(deleteFriend)
+
+
+module.exports = router;
